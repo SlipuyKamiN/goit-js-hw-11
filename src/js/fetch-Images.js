d78fe2fbd9da76a8ca256bbeaa6409ec4f844b0a
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-03-06 19:58:12 +0100

    makes all homeworks without btn actions

--- src/js/fetch-Images.js
+++ src/js/fetch-Images.js
@@ -1,28 +1,39 @@
 export class FetchImages {
-  constructor() {}
+  constructor() {
+    this.searchParams = {
+      key: '34149856-a909975f1401d97f20254d2ad',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: 1,
+      per_page: 40,
+      q: '',
+    };
+  }
 
-  searchParams = new URLSearchParams({
-    key: '34149856-a909975f1401d97f20254d2ad',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-    page: 1,
-  });
+  fetch() {
+    const { key, image_type, orientation, safesearch, page, q, per_page } =
+      this.searchParams;
 
-  fetch(q) {
-    return fetch(`https://pixabay.com/api/?${this.searchParams}&q=${q}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .catch(console.log);
+    return fetch(
+      `https://pixabay.com/api/?key=${key}&image_type=${image_type}&orientation${orientation}&safesearch=${safesearch}&page=${page}&per_page=${per_page}&q=${q}`
+    ).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
   }
   incrementPage() {
     this.searchParams.page += 1;
   }
   resetPage() {
     this.searchParams.page = 1;
+  }
+  setQuery(q) {
+    this.searchParams.q = q;
+  }
+  getQuery() {
+    return this.searchParams.q;
   }
 }
--- src/js/make-gallery.js
+++ src/js/make-gallery.js
@@ -1,44 +1,121 @@
 import { FetchImages } from './fetch-Images';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('#gallery'),
+  submitBtn: document.querySelector('#submit-btn'),
+  loadMoreBtn: document.querySelector('#load-more-btn'),
 };
 
 const getImages = new FetchImages();
-
+const showNotification = {
+  success(quantity) {
+    Notify.success(`Hooray! We found ${quantity} images.`);
+  },
+  noMatchingImages() {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  },
+  noSearchQuery() {
+    Notify.failure('Please input your search query!');
+  },
+  endOfResults() {
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  },
+};
 const makeMarkup = ({ hits }) => {
   return hits
     .map(img => {
       return `<div class="photo-card">
-  <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      ${img.likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      ${img.views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      ${img.comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      ${img.downloads}
-    </p>
-  </div>
-</div>`;
+      <a class="photo-link" href="${img.largeImageURL}">
+      <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${img.likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${img.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${img.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${img.downloads}
+        </p>
+      </div>
+    </a>
+</div>
+`;
     })
     .join('');
 };
 const renderMarkup = imgs => {
   const markup = makeMarkup(imgs);
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+  let gallery = new SimpleLightbox('.gallery a');
+};
+const scrollPage = () => {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 };
 
-getImages.fetch('toyota').then(imgs => {
-  renderMarkup(imgs);
-});
+const handleSearchSubmit = event => {
+  event.preventDefault();
+  const { searchQuery } = event.target.elements;
+
+  if (searchQuery.value === '') {
+    showNotification.noSearchQuery();
+    return;
+  }
+
+  refs.gallery.innerHTML = '';
+  refs.loadMoreBtn.classList.add('is-hidden');
+  getImages.setQuery(searchQuery.value);
+  getImages
+    .fetch()
+    .then(imgs => {
+      if (imgs.totalHits === 0) {
+        showNotification.noMatchingImages();
+        return;
+      }
+
+      showNotification.success(imgs.totalHits);
+      renderMarkup(imgs);
+      scrollPage();
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      getImages.resetPage();
+    })
+    .catch(console.log);
+};
+const handleLoadMoreBtnClick = () => {
+  getImages.incrementPage();
+  getImages
+    .fetch()
+    .then(imgs => {
+      if (imgs.hits.length < 40) {
+        showNotification.endOfResults();
+        refs.loadMoreBtn.classList.add('is-hidden');
+      }
+      renderMarkup(imgs);
+      scrollPage();
+    })
+    .catch(console.log());
+};
+
+refs.form.addEventListener('submit', handleSearchSubmit);
+refs.loadMoreBtn.addEventListener('click', handleLoadMoreBtnClick);
